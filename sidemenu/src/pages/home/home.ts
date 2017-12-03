abebf3a429baa2043bb83f08b2069d3728252437
David East <daithiwest@gmail.com>
2017-12-03 20:57:38 +0000

    SearchBar for played Artists(dummy data)

--- sidemenu/src/pages/home/home.ts
+++ sidemenu/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -10,4 +12,17 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
   }
   
+// alert to notify user to login using spotify
+//   constructor(public alertCtrl: AlertController) {
+//   }
+//   showAlert() {
+//     let alert = this.alertCtrl.create({
+//       title: 'Please Login!',
+//       subTitle: 'Please login using you Spotify Username and Pasword',
+//       buttons: ['OK']
+//     });
+//     alert.present();
+// }
 }
+  
+
