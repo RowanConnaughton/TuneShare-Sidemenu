fa70d065f5bc85f47433a4b86c330f81e54fcec8
David East <daithiwest@gmail.com>
2018-02-28 17:52:00 +0000

    merged

--- sidemenu/src/pages/show-article/show-article.ts
+++ sidemenu/src/pages/show-article/show-article.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Article} from '../article/article.model';
-<<<<<<< HEAD
-import { Nav } from 'ionic-angular';
-=======
 // import { Nav } from 'ionic-angular';
->>>>>>> 7e2f0f55922f8e2520325b970fa72bba4174a03d
 // import { HomePage } from '../home/home';
 /**
  * Generated class for the ShowArticleComponent component.
