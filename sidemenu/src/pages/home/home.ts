7f77c546b4f8572517c83694c3d9af0b259e8b13
David East <daithiwest@gmail.com>
2018-03-05 18:09:27 +0000

    profile data

--- sidemenu/src/pages/home/home.ts
+++ sidemenu/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component, DoCheck } from '@angular/core';
 import { NavController } from 'ionic-angular';
-// import { AlertController } from 'ionic-angular';
 import * as $ from 'jquery';
+
 
 @Component({
   selector: 'page-home',
@@ -10,44 +10,61 @@ import * as $ from 'jquery';
 export class HomePage implements DoCheck{
 
   ngDoCheck(){
-   
+    
+    
+     
 
-  
+   
 
 if( window.location.hash ) {
      var access_token = window.location.hash.split("=")[1].split("&")[0];
+
+    // Save data to sessionStorage
+     sessionStorage.setItem('access_token', access_token);
+
       $.ajax(
       {
         method:"GET",
-        url: "https://api.spotify.com/v1/search?q=roadhouse%20blues&type=track",
+        url: "https://api.spotify.com/v1/me",
         headers: {  
            'Authorization': 'Bearer '+ access_token
         },
         success: function(result){
           //handle
-          alert(JSON.stringify(result));
+         
+        $("#logIn").hide();
+        $('#card').show();
+
+          $("#name").html("Name: "+result.display_name);
+          $("#id").html("ID: "+result.id);
+          $("#email").html("Email: "+result.email);
+          $("#country").html("Country: "+result.country);
+          $("#img").attr("src", result.images[0].url);
         },
         error: function(result){
           alert("spotify fail");
           alert(JSON.stringify(result));
+          $('#user-profile').hide();
         }
-      });
+    });
   }
-}
+  // Save data to sessionStorage
+
+
+// // Get saved data from sessionStorage
+// var data = sessionStorage.getItem('key');
+
+// // Remove saved data from sessionStorage
+// sessionStorage.removeItem('key');
+
+// // Remove all saved data from sessionStorage
+// sessionStorage.clear();
+  
+ }
 constructor(public navCtrl: NavController) {
 
   }
-// alert to notify user to login using spotify
-//   constructor(public alertCtrl: AlertController) {
-//   }
-//   showAlert() {
-//     let alert = this.alertCtrl.create({
-//       title: 'Please Login!',
-//       subTitle: 'Please login using you Spotify Username and Pasword',
-//       buttons: ['OK']
-//     });
-//     alert.present();
-// }
+
 }
   
 
