6bc9f3783e52de5214757ff829dcf3491ec56349
David East <daithiwest@gmail.com>
2018-03-12 18:30:19 +0000

    format pages

--- sidemenu/src/pages/contact/contact.ts
+++ sidemenu/src/pages/contact/contact.ts
@@ -15,47 +15,47 @@ export class ContactPage implements OnInit {
   }
 
   ngOnInit() {
-    
-        this.rForm = new FormGroup({
-          
-          firstName: new FormControl('', {
-                  validators: Validators.required,
-                  updateOn: 'blur'
-                }),
-          
-                lastName: new FormControl('', {
-                  validators: Validators.required,
-                  updateOn: 'blur'
-                }),
-          
-                email: new FormControl('', {
-                 validators: [ Validators.email, Validators.required],
-                  updateOn: 'blur'
-                }),
-          
-                message: new FormControl('', {
-                  validators: Validators.required,
-                  updateOn: 'change'
-                })
-          
-          
-              })
-              
-              
-      }
-    
-      onSubmit() {
-        if (this.rForm.valid) {
-          console.log("Form Submitted!");
-          this.rForm.reset();
-        }
-      }
-    
-      addPost(post: any) {
-        this.rForm.controls.firstName =post.firstName;
-        this.rForm.controls.lastName = post.lastName;
-        this.rForm.controls.email = post.email;
-        this.rForm.controls.message = post.message;
-      }
-  
+
+    this.rForm = new FormGroup({
+
+      firstName: new FormControl('', {
+        validators: Validators.required,
+        updateOn: 'blur'
+      }),
+
+      lastName: new FormControl('', {
+        validators: Validators.required,
+        updateOn: 'blur'
+      }),
+
+      email: new FormControl('', {
+        validators: [Validators.email, Validators.required],
+        updateOn: 'blur'
+      }),
+
+      message: new FormControl('', {
+        validators: Validators.required,
+        updateOn: 'change'
+      })
+
+
+    })
+
+
+  }
+
+  onSubmit() {
+    if (this.rForm.valid) {
+      console.log("Form Submitted!");
+      this.rForm.reset();
+    }
+  }
+
+  addPost(post: any) {
+    this.rForm.controls.firstName = post.firstName;
+    this.rForm.controls.lastName = post.lastName;
+    this.rForm.controls.email = post.email;
+    this.rForm.controls.message = post.message;
+  }
+
 }
